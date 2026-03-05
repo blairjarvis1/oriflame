dfe1ad2d67145876a38240f1ef08b57f81324058
blairjarvis1 <blairjarvis@me.com>
2026-03-05 08:36:05 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const DIR = __dirname;
+
+const MIME = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'application/javascript',
+  '.json': 'application/json',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+  '.ico':  'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  const rawPath = req.url.split('?')[0];
+  let urlPath = rawPath === '/' ? '/beauty-homepage.html' : rawPath;
+  const filePath = path.join(DIR, urlPath);
+  const ext = path.extname(filePath);
+  const mime = MIME[ext] || 'text/plain';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404); res.end('Not found');
+    } else {
+      res.writeHead(200, { 'Content-Type': mime });
+      res.end(data);
+    }
+  });
+}).listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
